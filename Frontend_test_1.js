c67797bd5d2896fb57a1bbf57935252648e8d2ae
parthtagadiya99 <parthtagadiya9199@gmail.com>
2020-10-19 16:12:16 +0530

    Done

---
+++ Frontend_test_1.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+
+
+    $("#changeImg").click(function(){
+        $("#computer2").toggle(0);
+        $("#computer2b").toggle(0);
+    })
+
+    $("#flip").click(function () {
+        $("#flipDiv").toggleClass("flex-row-reverse")
+    })
+
+
+})
+
+    
+
